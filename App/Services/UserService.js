6d5855195336e347ec19087e4efbca6fdb2deac8
Furkan Kilic <furkan.kilic@estadyo.com>
2020-04-04 17:55:35 +0300

    config removed

--- App/Services/UserService.js
+++ App/Services/UserService.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { Config } from 'App/Config'
 import { is, curryN, gte } from 'ramda'
 
 const isWithin = curryN(3, (min, max, value) => {
@@ -14,10 +13,7 @@ const in200s = isWithin(200, 299)
  * Feel free to remove this example from your application.
  */
 const userApiClient = axios.create({
-  /**
-   * Import the config from the App/Config/index.js file
-   */
-  baseURL: Config.API_URL,
+  baseURL: 'https://google.com',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
